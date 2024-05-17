afc6bfcefa8330ffa5966c9ce561acc74fa04186
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-17 12:38:33 +0300

    Add create variable

---
+++ index.js
@@ -0,0 +1,58 @@
+console.log('hi!');
+
+//створіть змінну, яка буде зберігати кількість днів у тижні
+//виведіть цю змінну в консоль
+
+const amountDaysInWeek = 7;
+const days_in_week = 7;
+const allDayWeek = 7;
+console.log(allDayWeek);
+
+// створіть змінну, яка містить булеве значення чи є ви працівником
+const isWork = false;
+
+// створіть змінну, яка зберігає зріст в метрах 1.55
+const myHeight = 1.84;
+console.log(myHeight);
+
+// створіть змінну, яка говорить про те чи є у вас автомобіль
+const isCar = true;
+
+
+
+
+
+
+
+
+let titleMovie = 'Titanic';
+console.log(titleMovie);
+titleMovie = 'Titanic2';
+
+//створіть змінну для вашої домашньої тварини і запишіть в змінну що це за тварина я або null
+let myAnimal;
+myAnimal = 'cat';
+console.log(myAnimal);
+
+let firstName = 'Olena';
+console.log(firstName);
+
+let isAdult = true;
+let isWeekend = false;
+
+//створіть змінну яка буде містити булеве значення про те чи є ви студентом
+let isStudent = false;
+
+let yearOfBirth = 1900;
+console.log(yearOfBirth);
+yearOfBirth = 1978;
+console.log(yearOfBirth);
+
+// створіть змінну для зберігання вашого улюбленого кольору
+let colorFavorite = 'pink';
+console.log(firstName, colorFavorite);
+
+let age;
+console.log(age);
+age = 45;
+console.log(age);
