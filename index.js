a7f6e4f24c902515720a4b9513619aec632f9878
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-20 17:40:21 +0300

    Add confirm

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
-// const number1 = prompt('enter first number', 5);
-// const number2 = prompt('enter second number', 3);
-// console.log(' number1 = ', number1, typeof number1);
-// console.log(' number2 = ', number2, typeof number2);
+// const userName = prompt("Введіть ваше ім'я");
+// const isUserName = confirm("Це ваше ім'я - " + userName + '?');
 
-// // + - concatenation
-// const summa1 = number1 + number2; //53 - склеєні рядки
-// alert('summa1 = ' + summa1);
+// if (isUserName) {
+//   alert('Hi, ' + userName + '!!!');
+// } else {
+//   alert('Наступного разу будьте уважніше!');
+// }
 
-// const summa2 = +number1 + +number2; //8
-// alert('summa2 = ' + summa2);
 
-// // Number() - приводить до типу число (number)
-// const summa3 = Number(number1) + Number(number2); //8
-// alert('summa3 = ' + summa3);
-
-// const substruct = Number(number1) - Number(number2); //2
-// alert(substruct);
-
-// користувач послідовно вводить назву продукта і його ціну
-// так треба ввести інформацію про мінімум 2 продукта
-// вивести чек у вигляді, використовуючи один alert
-/*
-Ви купили:
-молоко - 45грн
-хліб - 23грн
-------
-до сплати - 68грн
-*/
-
-const productName1 = prompt('Enter name first product');
-const productPrice1 = prompt('Enter price ' + productName1);
-const productName2 = prompt('Enter name second product');
-const productPrice2 = prompt('Enter price ' + productName2);
-
-const totalPrice = Number(productPrice1) + Number(productPrice2);
-
-const productInfo1 = productName1 + ' - ' + productPrice1 + 'грн\n';
-const productInfo2 = productName2 + ' - ' + productPrice2 + 'грн\n';
-const totalInfo = '-------\nдо сплати - ' + totalPrice + 'грн\n';
-
-alert('Ви купили:\n' + productInfo1+productInfo2+totalInfo);
+// запитати у користувача що він хоче оримати на день народження
+// запитати підтвердження чи не передумав він отримати введений подарунок
+// якщо користувач підтвердив - виводимо Добре, пішов шукати твій подарунок ...
+// якщо користувач не підтвердив - виводимо Добре, подумай ще
 
