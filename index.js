ba72728ec03f2558754b84a5d6b60716aa4e938c
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-29 16:52:59 +0300

    Add check user input number

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-// false
-// undefined
-console.log(Boolean(undefined)); //false
-// null
-console.log(Boolean(null)); //false
-// ''  ""  ``
-console.log(Boolean('')); //false
-// 0  +0  -0
-console.log(Boolean(0)); //false
-// NaN  - помилка обчислення
-console.log(Boolean(NaN)); //false
+const userInput = prompt('Enter hour');
+const userNumber = Number(userInput); //NaN
 
+// перша помилка - користувач натиснув відміну
+const error1 = userInput === null;
+// друга помилка - користувач надіслав порожній рядок
+const error2 = userInput === '';
+// третя помилка - користувач замість цифр ввів літери
+const error3 = Number.isNaN(userNumber);
 
-// ! NOT  інверсія
-console.log(!true); //false
-console.log(!false); //true
-
-const str = '';
-console.log(!str); //true  !false
-
-console.log(!undefined); // !false -> true
+if (error1 || error2 || error3) {
+  alert('Error.\n' + userInput + ' - not a number!!!');
+} else {
+  if (userNumber >= 0 && userNumber <= 23) {
+    alert('thanks');
+    //
+  } else {
+    alert('Error.\n' + userInput + ' - not a hour!!!');
+  }
+}
