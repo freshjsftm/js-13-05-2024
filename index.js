566a692b1c1a1ff6fdc0bd82233c042f10ac55f4
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-31 18:15:59 +0300

    Add example switch as ternary

--- index.js
+++ index.js
@@ -1,56 +1,12 @@
-const message = `Введи мову, якою хочеш спілкуватися:
-1) українська - ua
-2) англійська - en
-3) польска - pl
-4) іспанська - es
-5) французька - fr
-`;
-// debugger
-const lang = prompt(message);
-
-switch (lang) {
-  case 'ua':
-  case '1': 
-  case 'українська':  
-    alert('привіт');
+switch (confirm('ви хочете покинути сайт?')) {
+  case true:
+    alert('до побачення');
     break;
-  case '2':
-  case 'англійська':    
-  case 'en':
-    alert('hi');
-    break;
-  case 'pl':
-  case '3': 
-  case 'польська':
-    alert('cześć');
-    break; 
-  case 'es':
-  case '4': 
-  case 'іспанська':
-    alert('hola');
-    break;
-  case 'fr':
-  case '5': 
-  case 'французька':
-    alert('bonjour');
-    break;         
-  default:
-    alert('мова не підтримується');
+  case false:
+    alert('ми раді, що ви лишаєтесь з нами');
     break;
 }
 
-
-
-// if (lang === 'ua' || lang==='1' || lang==='українська') {
-//   alert('привіт');
-// } else if (lang === 'en' || lang==='2' || lang==='англійська') {
-//   alert('hi');
-// } else if (lang === 'pl' || lang==='3' || lang==='польська') {
-//   alert('cześć');
-// } else if (lang === 'es' || lang==='4' || lang==='іспанська') {
-//   alert('hola');
-// } else if (lang === 'fr' || lang==='5' || lang==='французька') {
-//   alert('bonjour');
-// } else {
-//   alert('мова не підтримується');
-// }
+confirm('ви хочете покинути сайт?')
+  ? alert('до побачення')
+  : alert('ми раді, що ви лишаєтесь з нами');
