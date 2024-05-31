c9057847f3f6414feed7389f570e8fa88c79108c
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-05-31 17:53:29 +0300

    Add multichoose switch

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const message = `Введи мову, якою хочеш спілкуватися:
-українська - ua
-англійська - en
-польска - pl
-іспанська - es
-французька - fr
+1) українська - ua
+2) англійська - en
+3) польска - pl
+4) іспанська - es
+5) французька - fr
 `;
-
+// debugger
 const lang = prompt(message);
 
 switch (lang) {
   case 'ua':
+  case '1': 
+  case 'українська':  
     alert('привіт');
     break;
+  case '2':
+  case 'англійська':    
   case 'en':
     alert('hi');
     break;
   case 'pl':
+  case '3': 
+  case 'польська':
     alert('cześć');
     break; 
   case 'es':
+  case '4': 
+  case 'іспанська':
     alert('hola');
     break;
   case 'fr':
+  case '5': 
+  case 'французька':
     alert('bonjour');
     break;         
   default:
@@ -31,15 +41,15 @@ switch (lang) {
 
 
 
-// if (lang === 'ua') {
+// if (lang === 'ua' || lang==='1' || lang==='українська') {
 //   alert('привіт');
-// } else if (lang === 'en') {
+// } else if (lang === 'en' || lang==='2' || lang==='англійська') {
 //   alert('hi');
-// } else if (lang === 'pl') {
+// } else if (lang === 'pl' || lang==='3' || lang==='польська') {
 //   alert('cześć');
-// } else if (lang === 'es') {
+// } else if (lang === 'es' || lang==='4' || lang==='іспанська') {
 //   alert('hola');
-// } else if (lang === 'fr') {
+// } else if (lang === 'fr' || lang==='5' || lang==='французька') {
 //   alert('bonjour');
 // } else {
 //   alert('мова не підтримується');
