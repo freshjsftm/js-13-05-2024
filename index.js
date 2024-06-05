57bc8180a6c21285ebc8cc61811ab3d6bc7d8283
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-05 17:39:50 +0300

    Add while with update

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-const userInput1 = prompt('Enter first number ');
-const userInput2 = prompt('Enter second number ');
-const number1 = Number(userInput1);
-const number2 = Number(userInput2);
-const operator = prompt('Enter operator: +, -, *, /, %  ');
 
-switch (operator) {
-  case '+': {
-    const result = number1 + number2;
-    alert(number1 + '+' + number2 + '=' + result);
-    break;
-  }
-  case '-': {
-    const result = number1 - number2;
-    alert(number1 + '-' + number2 + '=' + result);
-    break;
-  }
-  case '*': {
-    const result = number1 * number2;
-    alert(number1 + '*' + number2 + '=' + result);
-    break;
-  }
-  case '/': {
-    const result = number1 / number2;
-    alert(number1 + '/' + number2 + '=' + result);
-    break;
-  }
-  case '%': {
-    const result = number1 % number2;
-    alert(number1 + '%' + number2 + '=' + result);
-    break;
-  }
-  default:
-    alert('operator not exists');
-    break;
+let amountPlate = 0;
+const totalAmountPlate = 5;
+
+// while (amountPlate>0) {
+//   console.log('washing plate ', amountPlate);
+//   amountPlate--; //decrement
+// }
+
+while (amountPlate < totalAmountPlate) {
+  amountPlate++;
+  console.log('washing plate ', amountPlate);
 }
