3f86e700886c9a795e9fe2580f88025077342f3c
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-07 17:21:54 +0300

    Add example loop for & while

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const secretNumber = 4;
-//якщо введене число більше - секретне менше
-//якщо введене число менше - секретне більше
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
 
-while (true) {
-  const userInput = prompt('enter number');  //qwerty
-  const userNumber = Number(userInput);      //NaN   ->   Number.isNaN(NaN)
 
-  if (userInput.trim() === '' || userInput === null || Number.isNaN(userNumber)) {
-    alert('error! enter number');
-  } else if (userNumber === secretNumber) {
-    alert('you win!');
-    break;
-  } else if (userNumber > secretNumber) {
-    alert('secret number less');
-  } else {
-    alert('secret number bigger');
-  }
+
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
 }
