3f5a5f9f4725eba2319f31fa3bd78620ebc92ebe
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-07 18:00:03 +0300

    Add summa numbers in diapazon

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-for (let i = 0; i < 5; i++) {
-  console.log(i);
+// потрібно порахувати суму усіх чисел в діапазоні
+// start - включно
+// end - не включно
+
+const start = 5;
+const end = 10;
+
+let summa =0;
+for (let i = start; i < end; i++) {
+  
+  // console.log(i);
+  summa += i; //summa = summa +i
+  console.log(summa);
 }
-
-
-
-let i = 0;
-while (i < 5) {
-  console.log(i);
-  i++;
-}
+console.log('summa = ', summa);
