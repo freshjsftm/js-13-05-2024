f9402c6fd4909501070db0b9fd6a615c709f8cbd
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-10 17:24:55 +0300

    Add continue

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const password = '123qwe';
-const maxTry = 3;
+const start = 15;
+const end = 20;
+const secretNumber = 17;
 
-for (let i = 0; i < maxTry; i++) {
-  const userInputPass = prompt('Enter password');
-  if (userInputPass === password) {
-    alert('Welcome!');
+for (let i = start; i < end; i++) {
+  if(i === secretNumber){
+    continue;
+  }
+  console.log(i);
+}
+
+debugger
+while(true){
+  const userInput =  prompt('Enter number');
+  if(userInput === null){
+    alert('будь уважний!');
+    continue;
+  }
+  if(Number(userInput) === secretNumber){
+    alert('Win!');
     break;
   }
-  alert('у вас лишилося ще ' + (maxTry - i - 1) + ' спроби');
 }
