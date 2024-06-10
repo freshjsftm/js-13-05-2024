8bb7e4990210effa32e4472c2b43c1d7ca838d71
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-10 17:45:11 +0300

    Add Шаблонний рядок

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const start = 15;
-const end = 20;
-const secretNumber = 17;
+const number1 = 2;
+const number2 = 7;
+const result = number1 * number2;
 
-for (let i = start; i < end; i++) {
-  if(i === secretNumber){
-    continue;
-    alert('after continue'); //цей рядок не виконається!!!
-  }
-  console.log(i);
-}
 
-// debugger
-while(true){
-  const userInput =  prompt('Enter number');
-  if(userInput === null){
+while (true) {
+  const userInput = prompt(`скільки буде 
+  ${number1} * ${number2} 
+  ?`);
+  if (userInput === null || userInput.trim()==='') {
     alert('будь уважний!');
     continue;
   }
-  if(Number(userInput) === secretNumber){
-    alert('Win!');
+  if (Number(userInput) === result) {
+    alert('Win! відповідь = ' + result);
     break;
   }
+  alert('Спробуй ще!');
 }
