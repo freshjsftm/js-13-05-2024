3b1c405c2afe0b73e23433f54cf4f17813f19bd0
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-12 17:36:12 +0300

    Add function with parametr

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+function logMessage(message) {
+  console.log(message);
+}
+logMessage(123);
+logMessage('Message from code');
 
-// function call
-logHi();
 
-// function declaration
-function logHi() {
-  console.log('Hi!');
+function logForUser(userName, word) {
+  console.log(`${word}, ${userName}!`);
 }
 
-// function call
-logHi();
+logForUser('Hi', 'Bill'); 
+logForUser('Tom', 'Ciao'); 
+logForUser('Anna', 'Halo'); 
+
+//порядок створення параметрів в функції повинен збігатися з порядком передачі аргументів в функцію
 
