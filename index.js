e021d6a0526fcd0a94d446e5ef13caefc62a36b1
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-14 17:44:01 +0300

    Add function with return

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-
 /**
- * draw multiple times thing
- * @param {number} times default value 1
- * @param {string} thing default value ':)'
+ *
+ * @param {number} number1
+ * @param {number} number2
+ * @returns {number}
  */
-function drawThings(times=1, thing=':)') {
-  for (let i = 1; i <= times; i++) {
-    console.log(i, thing);
-  }
+function addTwoNumbers(number1 = 0, number2 = 0) {
+  const summa = number1 + number2;
+  return summa;
 }
 
+const result = addTwoNumbers(7, 2);
+console.log(result);
 
+console.log(addTwoNumbers(4, 9));
 
-drawThings(2, '|^|^|');
-drawThings(5);
-drawThings(undefined,'=^.^=');
-drawThings();
+console.log(addTwoNumbers());
+
+//написати функцію, яка приймає в якості параметра сторону квадарата і повертає розраховану площу
+
+/**
+ * розраховує площу квадрата
+ * @param {number} side
+ * @returns {number}
+ */
+function getAreaSquare(side) {
+  const area = side * side;
+  return area;
+}
+
+// написати функцію, яка приймає два числа - початок і кінець діапазону (старт включно, кінець НЕ включно) і повертає функція сумму усіх чисел цього діапазону
+/**
+ *
+ * @param {number} start
+ * @param {number} end
+ * @returns {number}
+ */
+function summaDiapazonNumbers(start, end) {
+  let summa = 0;
+  for (let i = start; i < end; i++) {
+    summa += i;
+  }
+  return summa;
+}
+console.log(summaDiapazonNumbers(1, 6));
+
