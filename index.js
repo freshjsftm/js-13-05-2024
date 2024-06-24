6b9995faf6830b51215cf07528ca52bbd87982c3
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-24 17:52:53 +0300

    Add prototype with constructor

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const obj = {}; // new Object()
-console.log(obj);
-
 function User(name, age, isMale) {
   this.name = name;
   this.age = age;
   this.isMale = isMale;
 }
-
-//статична властивість
-User.prototype.getName = function() {
-  return this.name;
-}
-User.prototype.getInfo = function() {
-  return `${this.name} is ${this.age} years old`;
-}
-User.prototype.getGender = function() {
+function UserPrototype(){
+  this.getName = function() {
+    return this.name;
+  }
+  this.getInfo = function() {
+    return `${this.name} is ${this.age} years old`;
+  }
+  this.getGender = function() {
     return this.isMale ? 'male' : 'female';
+  }
 }
+//статична властивість
+User.prototype = new UserPrototype();
 
 const user1 = new User('Alex', 20, true);
 console.log(user1);
-
 console.log(user1.getInfo());
 console.log(user1.getGender());
-const user2 = new User('Maria', 23, false);
-console.log(user2.getName());
-console.log(user2);
+console.log(user1.getName());
+
