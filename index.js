5527a65e9af83bfdbf8eb28c12315067f0fa5ff2
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-24 17:32:58 +0300

    Add prototype object

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
-// constructor
-/**
- * 
- * @param {string} name 
- * @param {number} age 
- */
-function User(name, age){
+const obj = {}; // new Object()
+console.log(obj);
+
+function User(name, age, isMale) {
   this.name = name;
   this.age = age;
-  this.getInfo = function(){
-    return `${this.name} is ${this.age} years old`
-  }
+  this.isMale = isMale;
 }
+const UserPrototype = {
+  getName() {
+    return this.name;
+  },
+  getInfo() {
+    return `${this.name} is ${this.age} years old`;
+  },
+  getGender() {
+    return this.isMale ? 'male' : 'female';
+  },
+};
+//статична властивість
+User.prototype = UserPrototype;
 
-// instance
-const user1 = new User('Fred', 12);
-// console.log(user1.getInfo());
-const user2 = new User('Anna', 15);
-// console.log(user2.getInfo());
+const user1 = new User('Alex', 20, true);
+console.log(user1);
 
-// створити функцію конструктор Phone 
-// properties: brand, model, price
-// method: getDetails 'brand model costs $price'
-// створити інстанс 
-// викликати метод в консолі
-
-
-
-function Phone (brand, model, price){
-  this.brand = brand;
-  this.price = price;
-  this.model = model;
-  this.getDeteils = function(){
-      return `${this.brand}, ${this.model}, ${this.price}`;
-  }
-}
-
-// debugger
-const phone1 = new Phone('Google','mm',51000);
-console.log(phone1.getDeteils());
-
-const phone2 = new Phone('Sony','xp', 16000);
-console.log(phone2.getDeteils());
+console.log(user1.getInfo());
+console.log(user1.getGender());
+const user2 = new User('Maria', 23, false);
+console.log(user2.getInfo());
+console.log(user2);
