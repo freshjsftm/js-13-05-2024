1740297f7e7f2dabf785dc48561754226887884c
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-24 17:36:48 +0300

    Add prototype 2 variant

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ function User(name, age, isMale) {
   this.age = age;
   this.isMale = isMale;
 }
-const UserPrototype = {
-  getName() {
-    return this.name;
-  },
-  getInfo() {
-    return `${this.name} is ${this.age} years old`;
-  },
-  getGender() {
-    return this.isMale ? 'male' : 'female';
-  },
-};
+
 //статична властивість
-User.prototype = UserPrototype;
+User.prototype.getName = function() {
+  return this.name;
+}
+User.prototype.getInfo = function() {
+  return `${this.name} is ${this.age} years old`;
+}
+User.prototype.getGender = function() {
+    return this.isMale ? 'male' : 'female';
+}
 
 const user1 = new User('Alex', 20, true);
 console.log(user1);
@@ -26,5 +24,5 @@ console.log(user1);
 console.log(user1.getInfo());
 console.log(user1.getGender());
 const user2 = new User('Maria', 23, false);
-console.log(user2.getInfo());
+console.log(user2.getName());
 console.log(user2);
