f7cc2f9a7982d2b3088e42ec8f01ea247fe52c85
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-26 18:28:22 +0300

    Add push, pop, unshift, shift

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const array1 = [3];
-console.log(array1);
-
-// одне число в Array - буде позначати довжину масива з попрожніми комірчинками
-const array2 = new Array(3);
-console.log(array2);
-
-const array3 = new Array(3,4);
-console.log(array3);
 
 // створити масив значеннями якого будуть імена 3 ваших друзів
-
-const names = ['Neo','Alex','Bred'];
+const names = ['Neo','Alex'];
 console.log(names.length);
+
+// додати новий елемент в кінець масиву
+names.push('Anna','Olga');
+console.log(names);
+
+// додати новий елемент на початок масиву
+names.unshift('Max', 'Fred');
+console.log(names);
+
+// видалити елемент з кінця масиву
+const lastName = names.pop();
+console.log(lastName);
+console.log(names);
+
+// видалити елемент з початку масиву
+const firstName = names.shift();
+console.log(firstName);
+console.log(names);
