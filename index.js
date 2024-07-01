3b04241f8a3d2ae4dde255a3dcf803cd5e021da0
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-01 18:54:57 +0300

    Add forEach, map, filter, reduce

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const site = {
-  title:'my site',
-  headers:['qwe1','qwe2','qwe3'],
-  showHeaders(){
-    this.headers.forEach(function(elem){
-      console.log(elem);
-    })
-  },
-    showHeadersAndTitle(){
-    this.headers.forEach((elem)=>{
-      console.log(elem, this.title);
-    })
-  },
-  showHeadersAndTitle2(){
-    this.headers.forEach(function(elem){
-      console.log(elem, this.title);
-    })
-  },
-}
+// forEach - перебирає елементи масива і з кожним виконує колбек - НЕ ЗМІНЮЄ масив!!
+const numbers = [1,2,3];
+console.log(numbers);
+// numbers.forEach((number, index, array)=>{
+//   console.log(number, index, array);
+// })
+// numbers.forEach((number)=>{
+//   console.log(number, number ** 2);
+// })
 
-site.showHeaders()
-site.showHeadersAndTitle()
-site.showHeadersAndTitle2()
+// map - перебирає елементи масива і для кожного виконує колбек - а саме - повертає нове значення - НЕ ЗМІНЮЄ джерельний масив!! а ПОВЕРТАЄ новий масив з новими значеннями
+const newArrayNumbers = numbers.map((elem) => elem * elem);
+console.log(newArrayNumbers);
+console.log(numbers);
+
+// filter -  перебирає елементи масива і для кожного виконує колбек - а саме перевіряє значення в умовній конструкції і якщо конструкція правдива, то повертає значення в новому масиві. НЕ ЗМІНЮЄ джерельний масив!!
+const evenNumbers = numbers.filter((item) => item%2 === 0 );
+console.log(evenNumbers);
+console.log(numbers);
+
+// reduce - перебирає елементи масива і додає кожне значення до аккумулятора(в одну торбочку) НЕ ЗМІНЮЄ джерельний масив!! а повертає одне значення яке було накопичене в акумуляторі
+const summa = numbers.reduce((accumulator, number)=>accumulator+number);
+console.log(summa);
+console.log(numbers);
