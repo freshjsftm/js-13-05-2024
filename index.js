f3bbe1576866a9ce3fe4314cf730577b63935067
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-05 17:24:43 +0300

    Add character methods

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-// forEach - перебирає елементи масива і з кожним виконує колбек - НЕ ЗМІНЮЄ масив!!
-const numbers = [1,2,3];
-console.log(numbers);
-// numbers.forEach((number, index, array)=>{
-//   console.log(number, index, array);
-// })
-// numbers.forEach((number)=>{
-//   console.log(number, number ** 2);
-// })
+//string
 
-// map - перебирає елементи масива і для кожного виконує колбек - а саме - повертає нове значення - НЕ ЗМІНЮЄ джерельний масив!! а ПОВЕРТАЄ новий масив з новими значеннями
-const newArrayNumbers = numbers.map((elem) => elem * elem);
-console.log(newArrayNumbers);
-console.log(numbers);
+const str1 = 'cat and dog';
+console.log(str1);
+console.log(str1.length);
 
-// filter -  перебирає елементи масива і для кожного виконує колбек - а саме перевіряє значення в умовній конструкції і якщо конструкція правдива, то повертає значення в новому масиві. НЕ ЗМІНЮЄ джерельний масив!!
-const evenNumbers = numbers.filter((item) => item%2 === 0 );
-console.log(evenNumbers);
-console.log(numbers);
+console.log(str1.includes('and'));
 
-// reduce - перебирає елементи масива і додає кожне значення до аккумулятора(в одну торбочку) НЕ ЗМІНЮЄ джерельний масив!! а повертає одне значення яке було накопичене в акумуляторі
-const summa = numbers.reduce((accumulator, number)=>accumulator+number);
-console.log(summa);
-console.log(numbers);
+const firstLetter = str1.charAt(0);
+console.log(firstLetter);
+
+const firstLetterCode = str1.charCodeAt(0);
+console.log(firstLetterCode);
+
+const lastLetter = str1.at(-1);
+console.log(lastLetter);
+
+console.log(str1.indexOf('cat'));
+console.log(str1.lastIndexOf('cat'));
+
