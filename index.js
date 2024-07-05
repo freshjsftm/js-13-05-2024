6e69a165405ee6fd28a64628a74affee5a8a1386
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-05 18:24:50 +0300

    Add tasks

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 //string
 
-const str1 = 'Cat aNd doG'; //user
-const str2 = 'cat and dog'; //db
-console.log(str1);
+const userName = '      bRad     '; // Brad
 
-const str1UpperCase = str1.toUpperCase();
+const userNameTrim = userName.trim().toLowerCase();
+console.log(userNameTrim);
+const firstLetter = userNameTrim.charAt(0).toUpperCase();
+console.log(firstLetter);
+const lastLetters = userNameTrim.slice(1);
+console.log(lastLetters);
 
-console.log(str1UpperCase);
+const finallyUserName = firstLetter + lastLetters;
+console.log(finallyUserName);
 
-if(str1.toLowerCase() === str2){
-  console.log('equal');
-}else{
-  console.log('not equal');
+// користувач вводить ім'я для змінної - якщо воно починається з цифри ми повторюємо ввод
+
+//const userNameVariable = ' 4qwerty '
+//прибрати пробіли trim
+//отримати код першого символа charCodeAt(0)
+//порівняти код з діапазоном 48-57 включно
+
+while (true) {
+  const userNameVariable = prompt("ім'я для змінної");
+  const codeFirstSymbol = userNameVariable.trim().charCodeAt(0);
+  if (codeFirstSymbol < 48 || codeFirstSymbol > 57) {
+    break;
+  }
 }
