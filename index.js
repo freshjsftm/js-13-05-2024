abe0a0ab9b8d76d8f01a9f8030bfc5bd956e955e
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-05 19:02:08 +0300

    Add task format

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 //string
 
-const userName = '      bRad     '; // Brad
+const fileName = 'title.qwerty.exe';
 
-const userNameTrim = userName.trim().toLowerCase();
-console.log(userNameTrim);
-const firstLetter = userNameTrim.charAt(0).toUpperCase();
-console.log(firstLetter);
-const lastLetters = userNameTrim.slice(1);
-console.log(lastLetters);
+const posLastDote = fileName.trim().lastIndexOf('.');
+const format = fileName.trim().slice(posLastDote + 1);
 
-const finallyUserName = firstLetter + lastLetters;
-console.log(finallyUserName);
-
-// користувач вводить ім'я для змінної - якщо воно починається з цифри ми повторюємо ввод
-
-//const userNameVariable = ' 4qwerty '
-//прибрати пробіли trim
-//отримати код першого символа charCodeAt(0)
-//порівняти код з діапазоном 48-57 включно
-
-while (true) {
-  const userNameVariable = prompt("ім'я для змінної");
-  const codeFirstSymbol = userNameVariable.trim().charCodeAt(0);
-  if (codeFirstSymbol < 48 || codeFirstSymbol > 57) {
+switch (format) {
+  case 'txt':
+  case 'doc':
+    console.log('текст');
     break;
-  }
+  case 'jpg':
+  case 'png':
+    console.log('зображення');
+    break;
+  case 'exe':
+    console.log('інсталяшка');
+    break;
+  case 'html':
+  case 'css':
+  case 'js':
+    console.log('веб');
+    break;
+  default:
+    console.log('невідомий формат');
+    break;
 }
