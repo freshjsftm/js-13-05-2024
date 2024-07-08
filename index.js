7b9497af7a9b3f5505beea1e99be32d9cc0db1cd
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-08 17:58:41 +0300

    Add toSorted & toReversed

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 //reverse, sort
 
-const numbers = [5, 172, 1, 10, 4, 2, 101, 22];
+const numbers = [5, 172, 1, 10];
+const numbersSorted = numbers.toSorted((a, b) => a - b);
+const numbersSortedReverse = numbersSorted.toReversed();
 
 console.log(numbers);
+console.log(numbersSorted);
+console.log(numbersSortedReverse);
+
+
 // метод sort ЗМІНЮЄ!!! поточний масив
-console.log(numbers.sort((a, b) => a - b)); //0  1  -1
-console.log(numbers);
+// console.log(numbers.sort()); 
+// console.log(numbers.sort((a, b) => a - b)); //0  1  -1 
+// console.log(numbers);
 
 // console.log(numbers.sort((a, b) => b - a));
 // console.log(numbers);
 
 // метод reverse ЗМІНЮЄ!!! поточний масив
-console.log(numbers.reverse());
-console.log(numbers);
+// console.log(numbers.reverse());
+// console.log(numbers);
