a793af752117d013916eecc75b8f9b6b203c00d9
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-15 17:23:10 +0300

    Add class syntax

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-// Map  Set
-// треба створити носу структуру, яка буде збурігати пари ключ-значення
-// ключем буде ім'я студента
-// значенням буде масив оцінок студента
-// написати функції
-// додає нового студента і порожній масив оцінок
-// додає студенту нову оцінку
+// class  2015
 
-const studensWithGrade = new Map();
-function addNewStudent(dictionary, studentName) {
-  dictionary.set(studentName, []);
-}
+//1
+// function User(name, age){
+//   this.name = name;
+//   this.age = age;
+// }
+//2
+// function UserProptotype(){
+//   this.logName = function(){
+//     console.log(this.name)
+//   }
+// }
+//3
+// User.prototype = new UserProptotype();
 
-function addStudentNewGrade(dictionary, studentName, grade) {
-  if (dictionary.has(studentName)) {
-    const grades = dictionary.get(studentName);
-    grades.push(grade)
-    dictionary.set(studentName, grades);
-  } else {
-    dictionary.set(studentName, [grade]);
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  logName() {
+    console.log(this.name);
   }
 }
 
-function getAverageGradeStudent(dictionary, studentName){
-  if(dictionary.has(studentName)){
-    const grades = dictionary.get(studentName);
-    const summaGrades = grades.reduce((sum, grade)=> sum+grade);
-    return summaGrades/grades.length;
-  }
-}
-
-addNewStudent(studensWithGrade, 'Alex');
-addStudentNewGrade(studensWithGrade, 'Brad', 12);
-addStudentNewGrade(studensWithGrade, 'Brad', 8);
-addStudentNewGrade(studensWithGrade, 'Brad', 11);
-addNewStudent(studensWithGrade, 'Anna')
-addStudentNewGrade(studensWithGrade, 'Anna', 9);
-addStudentNewGrade(studensWithGrade,'Bob',7)
-console.log(getAverageGradeStudent(studensWithGrade, 'Brad').toFixed(2))
-console.log(studensWithGrade);
+//4
+const user = new User('Alex', 23);
+console.log(user);
+user.logName();
