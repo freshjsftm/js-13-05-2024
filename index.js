625d1e34fc501e39af1001e489cc6d7f74459133
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-17 17:53:54 +0300

    Add setter getter throw

--- index.js
+++ index.js
@@ -1,59 +1,36 @@
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-    this.id = Book.count++;
+class Square {
+  //private - можна користуватися лише в межах класу
+  #side;
+  constructor(side) {
+    // викликаємо setter
+    this.side = side;
   }
-  static count = 0;
-  showInfo() {
-    return `"${this.title}" ${this.author}`;
+  // setter - встановити значення властивісті
+  set side(value) {
+    //number >0
+    if (typeof value !== 'number') {
+      throw new TypeError('type must be number!');
+    }
+    if (value <= 0) {
+      throw new RangeError('value mast be positive');
+    }
+    this.#side = value;
+  }
+  // getter - отримати значення властивості
+  get side() {
+    return this.#side;
+  }
+  getArea() {
+    return this.#side ** 2;
   }
 }
 
-class Reader {
-  constructor(name) {
-    this.name = name;
-    this.id = Reader.count++;
-  }
-  static count = 0;
-  showInfo() {
-    return `Name = ${this.name}
-      id = ${this.id}`;
-  }
-}
 
-class Library {
-  constructor() {
-    this.books = [];
-    this.readers = [];
-    this.booksAndReaders = [];
-  }
-  addBook(...book) {
-    this.books.push(...book);
-  }
-  addReader(...reader) {
-    this.readers.push(...reader);
-  }
-  addBookForReader(reader, book) {
-    this.booksAndReaders.push({ idReader: reader.id, idBook: book.id });
-  }
-  showAllBooks(){
-    this.books.forEach((elem)=>console.table(elem))
-  }
-}
-const book1 = new Book('It', 'King');
-const book2 = new Book('Autsider', 'King');
-const user1 = new Reader('Brad');
-const user2 = new Reader('Anna');
-const library = new Library();
-library.addBook(book1, book2);
-library.addReader(user1, user2);
-library.addBookForReader(user1, book2);
-library.addBookForReader(user2, book1);
-console.log(library);
-library.showAllBooks()
-
-// const book1 = new Book('It','King');
-// console.log(book1.showInfo())
-// const user = new Reader('Brad');
-// console.log(user.showInfo())
+///////////////////////////////
+const square = new Square(7);
+// викликаємо setter
+square.side = 3;
+// викликаємо getter
+console.log(square.side);
+// square.side = '!!!!';
+console.log(square.getArea());
